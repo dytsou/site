6b09d057fd70f6ac58420929e2cd4deaf15c559d
dytsou <d.y.tsou@gmail.com>
2026-07-12 14:41:50 +0800

    chore: remove cal Pages Function proxy
    /cal/ is routed by the Front Door Worker; the site Pages Function proxy
    to cal.tsou.me is no longer used.

--- functions/cal/[[path]].js
+++
@@ -1,6 +0,0 @@
-import { proxySubsite } from '../_proxy.js';
-
-const CAL_ORIGIN = 'https://cal.tsou.me';
-
-export const onRequest = (context) =>
-  proxySubsite(context, { origin: CAL_ORIGIN, stripPrefix: '/cal' });
